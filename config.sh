138589839e9505024a876737a94d691044da8595
josephsys1997 <98211248+josephsys1997@users.noreply.github.com>
2024-03-30 12:28:24 +0000

    Update config.sh
    I hope it works

--- config.sh
+++ config.sh
@@ -8,7 +8,7 @@ export BRANCH="twrp-11" # the branch of manifest
 # about your device
 export DEVICE="X697" # codename used in device tree
 export DT_LINK="https://github.com/twrpdtgen/android_device_infinix_Infinix-X697" # device tree link
-export DT_BRANCH="twrp" # device tree branch
+export DT_BRANCH="vnd_x697_h812-user-11-RP1A.200720.011-460220-release-keys" # device tree branch
 export VENDOR="infinix" # device manufacturer or vendor
 export TARGET="bootimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="twrp" # recovery type: omni, twrp, something else
