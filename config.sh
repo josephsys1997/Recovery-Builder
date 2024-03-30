195793f24c9d0f72c6e5dd5a5dcfac46075309d0
josephsys1997 <98211248+josephsys1997@users.noreply.github.com>
2024-03-30 13:37:42 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git" # the link of manifest
+export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp" # the link of manifest
 export BRANCH="twrp-11" # the branch of manifest
 
 # about your device
